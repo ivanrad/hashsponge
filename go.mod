147466c364ae9b5f30d4e7f608325532e5ac6fa1
ivanrad <ivanra@gmail.com>
2023-12-06 08:19:37 +0000

    initial commit

---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/ivanrad/hashsponge
+
+go 1.21.4
+
+require golang.org/x/crypto v0.16.0
+
+require golang.org/x/sys v0.15.0 // indirect
---
+++ hashsponge.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"crypto/md5"
+	"crypto/sha1"
+	"crypto/sha256"
+	"crypto/sha512"
+	"flag"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+
+	"golang.org/x/crypto/blake2b"
+)
+
+var hashAlg = flag.String("a", "sha256", "hash algorithms: md5, sha1, sha256, sha384, sha512, blake2b")
+
+func usage() {
+	fmt.Fprintf(flag.CommandLine.Output(), "Usage:  %s [-a HASH_ALGORITHM] HASH_VALUE\n\n", os.Args[0])
+	fmt.Fprintf(flag.CommandLine.Output(), "Soak up standard input, and write to standard output if checksum matches the given argument.\n\n")
+	flag.PrintDefaults()
+}
+
+func main() {
+	flag.Usage = usage
+	flag.Parse()
+
+	var hashFn func([]byte) []byte
+
+	switch strings.ToLower(*hashAlg) {
+	case "md5":
+		hashFn = func(buf []byte) []byte {
+			h := md5.Sum(buf)
+			return h[:]
+		}
+	case "sha1":
+		hashFn = func(buf []byte) []byte {
+			h := sha1.Sum(buf)
+			return h[:]
+		}
+	case "sha256":
+		hashFn = func(buf []byte) []byte {
+			h := sha256.Sum256(buf)
+			return h[:]
+		}
+	case "sha384":
+		hashFn = func(buf []byte) []byte {
+			h := sha512.Sum384(buf)
+			return h[:]
+		}
+	case "sha512":
+		hashFn = func(buf []byte) []byte {
+			h := sha512.Sum512(buf)
+			return h[:]
+		}
+	case "blake2b":
+		hashFn = func(buf []byte) []byte {
+			h := blake2b.Sum512(buf)
+			return h[:]
+		}
+	default:
+		fmt.Fprintf(os.Stderr, "error: unknown hash algorithm: %s\n", *hashAlg)
+		os.Exit(1)
+	}
+
+	hashArg := flag.Arg(0)
+	if hashArg == "" {
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	buf, err := io.ReadAll(os.Stdin)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		os.Exit(1)
+	}
+
+	inHash := fmt.Sprintf("%x", hashFn(buf))
+	if hashArg != inHash {
+		fmt.Fprintf(os.Stderr, "error: hash mismatch (input hash: %s)", inHash)
+		os.Exit(1)
+	}
+
+	nwritten := 0
+	for nwritten < len(buf) {
+		n, err := os.Stdout.Write(buf)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "error: %v\n", err)
+			os.Exit(1)
+		}
+		nwritten += n
+	}
+}
